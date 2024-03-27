f431054daec92c694a3cff2c7892f1056d140548
Alireza Asadi <malware1378@gmail.com>
2024-03-27 21:45:47 +0330

    first commit

---
+++ points.js
@@ -0,0 +1,60 @@
+const  points ={
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            51.48043364077077,
+            35.77495081683951
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            51.332556272809086,
+            35.65875295612527
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            51.476221893619936,
+            35.71493233734746
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            51.521320615159084,
+            35.725817679401075
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            51.36158928957761,
+            35.77864573010439
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
